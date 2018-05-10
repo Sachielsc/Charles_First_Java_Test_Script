388c504d10c969c20ddfa4e5dc15392e578101a8
Charles Shu <cshu@planittesting.com>
2018-05-10 13:22:00 +1200

    solved: "SimplePageTest" is now working

--- src/test/java/testsByJunit/SimplePageTest.java
+++ src/test/java/testsByJunit/SimplePageTest.java
@@ -3,6 +3,7 @@ package testsByJunit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utils.Driver;
@@ -13,15 +14,15 @@ import org.openqa.selenium.support.PageFactory;
 
 public class SimplePageTest {
 
-    // Initialization
-    WebDriver driver = Driver.getWebDriver();
+    // definitions
+    WebDriver driver;
     WebDriverWait wait;
     private String baseUrl = "https://alphav3.vaultintel.com/index/hostLogin";
     private String userName = "plan.6";
     private String passWord = "plan01#";
 
     // Web elements
-    @FindBy(id = "password")
+    @FindBy(id = "user")
     WebElement userNameField;
     @FindBy(css = "input#password")
     WebElement passWordField;
@@ -31,7 +32,8 @@ public class SimplePageTest {
     // Step definitions
     public void Init()
     {
-        // Navigate to Vault page
+        driver = Driver.getWebDriver();
+        wait = new WebDriverWait(driver, 8);
         driver.get(baseUrl);
         PageFactory.initElements(driver, this);
     }
@@ -45,6 +47,7 @@ public class SimplePageTest {
         wait.until(ExpectedConditions.elementToBeClickable(passWordField));
         passWordField.sendKeys(passWord);
 
+        // log in
         wait.until(ExpectedConditions.elementToBeClickable(loginButton));
         loginButton.click();
     }
