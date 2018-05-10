afb0f4b7094c8ceb275a3e6c1c6707ada18acd07
Charles Shu <cshu@planittesting.com>
2018-05-10 12:40:09 +1200

    add my first JUnit script "SimplePageTest" TODO (solved): element locator not working

--- src/test/java/testsByJunit/CalculatorTest.java
+++ src/test/java/testsByJunit/CalculatorTest.java
@@ -1,4 +1,4 @@
-package testsForJunit;
+package testsByJunit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import classesForJunit.Calculator;
@@ -10,18 +10,18 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class CalculatorTest {
 
     @Test
-    @DisplayName("1 + 1 = 2")
+    @DisplayName("Test whether \"1 + 1 = 2\"")
     void addTwoFixedNumbers() {
         Calculator calculator = new Calculator();
         assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
     }
 
-    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @ParameterizedTest(name = "Test whether \"{0} + {1} = {2}\"")
     @CsvSource({
-            "0,    1,   1",
-            "1,    2,   3",
-            "49,  51, 100",
-            "1,  100, 101"
+            "0, 1, 1",
+            "1, 2, 3",
+            "49, 51, 100",
+            "1, 100, 101"
     })
     void addTwoVariablesFromCsv(int first, int second, int expectedResult) {
         Calculator calculator = new Calculator();
