53e40bb5e6371a3d7d6b1056059d0a8131565461
Charles Shu <cshu@planittesting.com>
2018-05-10 16:50:51 +1200

    Working version 4.0 released!
    1. Solve problem "POM test not working": use JUnit 4 or JUnit Jupiter
    only!

--- src/test/java/testsByJunit/PageObjectModelTest.java
+++ src/test/java/testsByJunit/PageObjectModelTest.java
@@ -1,14 +1,15 @@
 package testsByJunit;
 
-import org.junit.Before;
+//The following is JUnit4
+//import org.junit.Before;
+//import org.junit.Test;
+
+//The following is JUnit5 (Beta)
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pageobjects.*;
 import utils.Driver;
 
@@ -16,14 +17,14 @@ public class PageObjectModelTest {
     // global variable definitions
     WebDriver driver;
 
-    @Before
-    public void setUp(){
+    @BeforeEach
+    public void setUp() {
         driver = Driver.getWebDriver();
     }
 
     @Test
     @DisplayName("My first POM / JUnit test case")
-    void TestCase1() {
+    public void TestCase1() {
         // variable definitions
         String userName = "plan.6";
         String passWord = "plan01#";
